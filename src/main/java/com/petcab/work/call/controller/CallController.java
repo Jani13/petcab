4d81902d3d8983bd7239b94006afb91c6ca44ddd
Janice <sohyohkim@gmail.com>
2021-04-22 12:30:10 +0900

    0422_push

--- src/main/java/com/petcab/work/call/controller/CallController.java
+++ src/main/java/com/petcab/work/call/controller/CallController.java
@@ -218,7 +218,7 @@ public class CallController {
 			@SessionAttribute(name="loginMember", required=false) Member loginMember,
 			@PathVariable int callNo,
 			HttpServletRequest request,
-			// @RequestParam(value="callNo", required=true) int callNo,
+			@RequestParam(value="callNo", required=true) int callNo,
 			@RequestParam(value="impUid", required=true) String impUid,
 			@ModelAttribute Call call,
 			ModelAndView model) {
